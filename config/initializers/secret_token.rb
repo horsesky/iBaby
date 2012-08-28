4b5cab87bbad9c28458f61abe903c664a3799d57
wutianyin <horsesky1226@gmail.com>
2012-08-28 23:13:40 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IBaby::Application.config.secret_token = '0821f3873960d402bc08f384e40341fff3bdee513c2cfadbcfb4a80cf1c64ad13513749d3ff2aa2948aa891f0afaa48e7dedb9b718e166c59e018ad5660cab3a'
